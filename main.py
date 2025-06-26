56a70391649b836abf50d9a129e41013b5a32bc2
pyPaulson <agyekumpaul07@gmail.com>
2025-06-26 23:31:24 +0000

    Changes

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def get_shipment():
+def get_all_shipment():
     return {"message": "Hello, World!"}   
+
+
+@app.get("/shipment")
+def get_shipment():
+    return {"Details": "This is shipment"}
